d08bcd5d1153e3fd43100ee33603e0b30679ec1f
ra11 <rajeswarivinayagam05@gmail.com>
2024-11-22 23:54:46 +0400

    updated bulkmail

---
+++ demo.js
@@ -0,0 +1,23 @@
+const fileInput=document.getElementById("fileInput")
+
+fileInput.addEventListener("change",(event)=>{
+   const file= event.target.files[0]
+    console.log(file)
+   const reader=new FileReader();
+
+   reader.onload=function(e){
+
+    const data=e.target.result;
+    const workbook = XLSX.read(data, { type: 'binary' })
+    const sheetName=workbook.SheetNames[0]
+    const worksheet=workbook.Sheets[sheetName]
+   const emaillist=XLSX.utils.sheet_to_json(worksheet,{header:'A'})
+    console.log(worksheet);
+   }
+
+   reader.readAsBinaryString(file);
+   
+
+   
+  
+})
